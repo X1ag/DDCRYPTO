63395b5210ddbe6b1034a7ebe88732d36d0a64e1
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2023-02-03 23:39:17 +0300

    DDCrypto полностью работает!
    Настроено все до мелочей, проект готов

--- main.py
+++ main.py
@@ -15,7 +15,28 @@ import asyncio
 import os
 import pickle
 from loguru import logger
+import threading
+import time
+import sqlite3
 
+# Переменная для функции auto_check_exchange(last_buy)
+last_buy = 0
+
+
+# Проверка есть ли юзер в базе данных
+def is_in_db(chat_id):
+    conn = sqlite3.connect('subscribers.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM subscribers WHERE chat_id=?", (chat_id,))
+    rows = cursor.fetchone()
+    if rows is not None:
+        return True
+    else:
+        return False
+
+
+# Конфигурация файла с токеном бота
 try:
     # Читаем переменную data из файла 'save.pkl'
     with open('save.pkl', 'rb') as read:
@@ -26,14 +47,65 @@ except:
     with open('save.pkl', 'wb') as write:
         pickle.dump(API_TOKEN, write)
 
+
+# Автоматическая проверка курса криптовалюты и информирование всех юзеров о его изменении, если оно равно, либо
+# превышает 2%
+def auto_check_exchange(last_buy):
+    r = requests.get('https://www.blockchain.com/ru/ticker').json()
+    if last_buy != 0:
+        if last_buy / float(r['RUB']['buy']) >= 1.02:
+            # Создание соединения с базой данных
+            conn = sqlite3.connect('subscribers.db')
+            cursor = conn.cursor()
+
+            # Выбор всех идентификаторов из таблицы
+            cursor.execute("SELECT user_id FROM subscribers")
+
+            # Получение всех идентификаторов в виде списка
+            ids = [x[0] for x in cursor.fetchall()]
+
+            # Цикл по всем идентификаторам
+            for usrid in ids:
+                requests.post(f'https://api.telegram.org/bot{API_TOKEN}/sendMessage?chat_id={usrid}&text=❗ Курс упал '
+                              f'на {(last_buy / float(r["RUB"]["buy"])) * 100 - 100}%')
+                asyncio.sleep(6)
+
+            # Закрытие соединения
+            conn.close()
+        elif float(r['RUB']['buy']) / last_buy >= 1.02:
+            # Создание соединения с базой данных
+            conn = sqlite3.connect('subscribers.db')
+            cursor = conn.cursor()
+
+            # Выбор всех идентификаторов из таблицы
+            cursor.execute("SELECT id FROM subscribers")
+
+            # Получение всех идентификаторов в виде списка
+            ids = [x[0] for x in cursor.fetchall()]
+
+            # Цикл по всем идентификаторам
+            for usrid in ids:
+                requests.post(f'https://api.telegram.org/bot{API_TOKEN}/sendMessage?chat_id={usrid}&text=❗ Курс вырос '
+                              f'на {(last_buy / float(r["RUB"]["buy"])) * 100 - 100}%')
+
+            # Закрытие соединения
+            conn.close()
+    last_buy = r['RUB']['buy']
+
+
+# Создание вторичного потока с проверкой (поток нужен во избежание конфликтов с ботом)
+t = threading.Timer(interval=900.0, function=auto_check_exchange, args=(last_buy,))
+t.start()
+
 # Конфигурация логирования
 loguru.logger.add(
-    "logs/{time:YYYY-MM-DD}.log",
+    "log.log",
     rotation="7 days",
     level="DEBUG"
 )
 
 
+# Фильтр Callback вызовов Inline-кнопок
 class CallbackDataFilter(Filter):
     def __init__(self, data: str):
         self.data = data
@@ -49,35 +121,42 @@ dispatcher = Dispatcher(bot, storage=storage)
 loguru.logger.debug('Бот был запущен')
 
 
+# Создание класса с переменными, которые будут принимать значение сообщения пользователя
 class Form(StatesGroup):
     block_id = State()
     addr = State()
 
 
-inline_keyboard = InlineKeyboardMarkup()
-
+'''Inline-клавиатуры'''
+# Создание кнопок для основной клавиатуры
 button_last = InlineKeyboardButton('🔍 Последний блок', callback_data='last')
 button_exchange = InlineKeyboardButton('💰 Курс Биткоина', callback_data='exchange')
 button_block = InlineKeyboardButton('🔗 Информация о блоке', callback_data='block')
 button_balance = InlineKeyboardButton('💼 Биткоин кошелек', callback_data='balance')
-inline_keyboard.add(button_last)
-inline_keyboard.add(button_exchange)
-inline_keyboard.add(button_block)
-inline_keyboard.add(button_balance)
 
+# Создание основной клавиатуры и внесение в нее кнопок
+start_menu = InlineKeyboardMarkup()
+start_menu.add(button_last)
+start_menu.add(button_exchange)
+start_menu.add(button_block)
+start_menu.add(button_balance)
+
+# Создание клавиатуры, для перенаправления в меню
 menu_keyboard = InlineKeyboardMarkup()
-
 menu_button_text = InlineKeyboardButton('◀️ В меню', callback_data='menu')
-
 menu_keyboard.add(menu_button_text)
 
+# Стикеры бота (вынесены для удобного редактирования)
 menu_sticker = 'CAACAgIAAxkBAAEHi25j2o75xyB-m8C3s4ITgCo7JWPEmQACph8AAv0RsEpbI3U8YSp1vy4E'
 last_sticker = 'CAACAgIAAxkBAAEHi4Nj2pISiR7ub3J2ZJoFKgpZLkCFagACKCkAAtjzqErya_sR6K2N4i4E'
 exchange_sticker = 'CAACAgIAAxkBAAEHi4Vj2pI1uvDP9bz6NoJuw0kX9rz7tQAC7i4AAu6RsEpU_iAgu_9-aS4E'
 block_sticker = 'CAACAgIAAxkBAAEHi4lj2pJS89tE80V_ZHZuAc2G046hYAACGSsAAgREqErosNZKKuXTDC4E'
 balance_sticker = 'CAACAgIAAxkBAAEHi5Zj2pUrlw0OKm70CCTKPpUB9KqN9gACVyUAAsoEqUp6NgKY6HXb-S4E'
 error_sticker = 'CAACAgIAAxkBAAEHi5Fj2pUO7WE5dh8RaOOAzL-5LslsIAACkCwAAvLrqUr7GbPrm5Xk9C4E'
+sub_sticker = 'CAACAgIAAxkBAAEHlDRj3WaudkWdYnI_-i9weZA5InsLWwACtSkAAnIHsEpATn7qYGoMBS4E'
+unsub_sticker = 'CAACAgIAAxkBAAEHlDZj3WbOv1Q5yN9stdn_z42jDyEQXwACkCwAAvLrqUr7GbPrm5Xk9C4E'
 
+# Переменная меню (вынесена для быстрого редактирования)
 menu = '''
 🔥 <b>Меню:</b>
 
@@ -96,11 +175,13 @@ async def start_handler(message: types.Message):
     user_id = message.from_user.id
     full_name = message.from_user.full_name
     username = message.from_user.username
+    message_id = message.message_id
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
     loguru.logger.info(f"Введена команда /start пользователем. Имя: {full_name}, ID: {user_id}, юзернейм: {username}")
     await bot.send_sticker(chat_id=chat_id,
                            sticker=menu_sticker)
     await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
-    await bot.send_message(chat_id=chat_id, text=menu, parse_mode='HTML', reply_markup=inline_keyboard)
+    await bot.send_message(chat_id=chat_id, text=menu, parse_mode='HTML', reply_markup=start_menu)
 
 
 @logger.catch
@@ -111,12 +192,15 @@ async def last_block_handler(message: types.Message):
     user_id = message.from_user.id
     full_name = message.from_user.full_name
     username = message.from_user.username
+    message_id = message.message_id
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
     loguru.logger.info(f"Введена команда /last пользователем. Имя: {full_name}, ID: {user_id}, юзернейм: {username}")
     await bot.send_sticker(chat_id=chat_id,
                            sticker=last_sticker)
     await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
+    wait_id = (await bot.send_message(chat_id, '⚙️ <b>Подготавливаем данные, это может занять некоторое время...</b>', parse_mode='HTML')).message_id
     text = f'{await last_block()}'
-    await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+    await bot.edit_message_text(text=text, chat_id=chat_id, message_id=wait_id, parse_mode='HTML')
     await bot.send_chat_action(chat_id, types.ChatActions.UPLOAD_DOCUMENT)
     await asyncio.sleep(1)
     await bot.send_document(chat_id=chat_id, document=open('transactions.html', 'rb'), reply_markup=menu_keyboard)
@@ -130,13 +214,15 @@ async def exchange_handler(message: types.Message):
     user_id = message.from_user.id
     full_name = message.from_user.full_name
     username = message.from_user.username
+    message_id = message.message_id
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
     loguru.logger.info(
         f"Введена команда /exchange пользователем. Имя: {full_name}, ID: {user_id}, юзернейм: {username}")
     await bot.send_sticker(chat_id=chat_id,
                            sticker=exchange_sticker)
     await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
     text = f'{await cryptocurrency_exchange_rate()}'
-    await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML', reply_markup=menu_keyboard)
+    await bot.send_photo(chat_id, 'https://www.tradingview.com/x/6R35t7Uy/', caption=text, parse_mode='HTML', reply_markup=menu_keyboard)
 
 
 @logger.catch
@@ -157,11 +243,13 @@ async def block_handler(message: types.Message, state: FSMContext):
                                               sticker=block_sticker)).message_id
         await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
         text = f'{await block_by_number(block_id)}'
-        await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+        wait_id = (await bot.send_message(chat_id, '⚙️ <b>Подготавливаем данные, это может занять некоторое время...</b>', parse_mode='HTML')).message_id
+        await bot.edit_message_text(text=text, chat_id=chat_id, message_id=wait_id, parse_mode='HTML')
         if text != '❌ Введен неверный ID блока!':
             await bot.send_chat_action(chat_id, types.ChatActions.UPLOAD_DOCUMENT)
             await asyncio.sleep(1)
-            await bot.send_document(chat_id=chat_id, document=open('transactions.html', 'rb'), reply_markup=menu_keyboard)
+            await bot.send_document(chat_id=chat_id, document=open('transactions.html', 'rb'),
+                                    reply_markup=menu_keyboard)
         else:
             await bot.delete_message(chat_id=chat_id, message_id=stickwel_id)
     except:
@@ -175,7 +263,7 @@ async def block_handler(message: types.Message, state: FSMContext):
 @dispatcher.message_handler(state=Form.block_id)
 async def process_block_id(message: types.Message, state: FSMContext):
     """
-    Process block_id
+    Обработка ID Блока
     """
     chat_id = message.chat.id
     msg_id = message.message_id
@@ -198,7 +286,8 @@ async def process_block_id(message: types.Message, state: FSMContext):
                                           sticker=block_sticker)).message_id
     await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
     text = f'{await block_by_number(block_id)}'
-    await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+    wait_id = (await bot.send_message(chat_id, '⚙️ <b>Подготавливаем данные, это может занять некоторое время...</b>', parse_mode='HTML')).message_id
+    await bot.edit_message_text(text=text, chat_id=chat_id, message_id=wait_id, parse_mode='HTML')
     if text != '❌ Введен неверный ID блока!':
         await bot.send_chat_action(chat_id, types.ChatActions.UPLOAD_DOCUMENT)
         await asyncio.sleep(1)
@@ -241,7 +330,7 @@ async def balance_handler(message: types.Message):
 @dispatcher.message_handler(state=Form.addr)
 async def process_addr(message: types.Message, state: FSMContext):
     """
-    Process address
+    Обработка BTC адресса
     """
     chat_id = message.chat.id
     msg_id = message.message_id
@@ -266,6 +355,8 @@ async def process_addr(message: types.Message, state: FSMContext):
 @dispatcher.message_handler(state='*', commands='cancel')
 @dispatcher.message_handler(Text(equals='отмена', ignore_case=True), state='*')
 async def cancel_handler(message: types.Message, state: FSMContext):
+    """Закрытие state в случае ввода /cancel"""
+    loguru.logger.info(f"Ожидание ввода данных прервано командой /cancel")
     current_state = await state.get_state()
     if current_state is None:
         return
@@ -277,15 +368,14 @@ async def cancel_handler(message: types.Message, state: FSMContext):
 @dispatcher.callback_query_handler(CallbackDataFilter(data='last'))
 async def process_callback_last(callback_query: CallbackQuery):
     """Хэндлер команды /last"""
+    loguru.logger.info(f"Нажата кнопка последнего блока пользователем")
     chat_id = callback_query.message.chat.id
-    user_id = callback_query.message.from_user.id
-    full_name = callback_query.message.from_user.full_name
-    username = callback_query.message.from_user.username
     await bot.send_sticker(chat_id=chat_id,
                            sticker=last_sticker)
     await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
+    wait_id = (await bot.send_message(chat_id, '⚙️ <b>Подготавливаем данные, это может занять некоторое время...</b>', parse_mode='HTML')).message_id
     text = f'{await last_block()}'
-    await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+    await bot.edit_message_text(text=text, chat_id=chat_id, message_id=wait_id, parse_mode='HTML')
     await bot.send_chat_action(chat_id, types.ChatActions.UPLOAD_DOCUMENT)
     await asyncio.sleep(1)
     await bot.send_document(chat_id=chat_id, document=open('transactions.html', 'rb'), reply_markup=menu_keyboard)
@@ -293,25 +383,22 @@ async def process_callback_last(callback_query: CallbackQuery):
 
 @dispatcher.callback_query_handler(CallbackDataFilter(data='exchange'))
 async def process_callback_exchange(callback_query: CallbackQuery):
-    """Хэндлер команды /exchange"""
+    """CallbackDataFilter кнопки exchange"""
+    loguru.logger.info(f"Нажата кнопка курс BTC пользователем")
     chat_id = callback_query.message.chat.id
     user_id = callback_query.message.from_user.id
-    full_name = callback_query.message.from_user.full_name
-    username = callback_query.message.from_user.username
     await bot.send_sticker(chat_id=chat_id,
                            sticker=exchange_sticker)
     await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
     text = f'{await cryptocurrency_exchange_rate()}'
-    await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML', reply_markup=menu_keyboard)
+    await bot.send_photo(chat_id, 'https://www.tradingview.com/x/6R35t7Uy/', caption=text, parse_mode='HTML', reply_markup=menu_keyboard)
 
 
 @dispatcher.callback_query_handler(CallbackDataFilter(data='block'))
 async def process_callback_block(callback_query: CallbackQuery):
+    """CallbackDataFilter кнопки block"""
+    loguru.logger.info(f"Нажата кнопка блок пользователем")
     chat_id = callback_query.message.chat.id
-    user_id = callback_query.message.from_user.id
-    full_name = callback_query.message.from_user.full_name
-    username = callback_query.message.from_user.username
-    msg_id = callback_query.message.message_id
     global msgg_id, stick_id, stickwel_id
     stick_id = (await bot.send_sticker(chat_id=chat_id,
                                        sticker=block_sticker)).message_id
@@ -322,11 +409,10 @@ async def process_callback_block(callback_query: CallbackQuery):
 
 
 @dispatcher.callback_query_handler(CallbackDataFilter(data='balance'))
-async def process_callback_block(callback_query: CallbackQuery):
+async def process_callback_balance(callback_query: CallbackQuery):
+    """CallbackDataFilter кнопки balance"""
+    loguru.logger.info(f"Нажата кнопка баланс пользователем")
     chat_id = callback_query.message.chat.id
-    user_id = callback_query.message.from_user.id
-    full_name = callback_query.message.from_user.full_name
-    username = callback_query.message.from_user.username
     msg_id = callback_query.message.message_id
     global command
     command = msg_id
@@ -340,15 +426,76 @@ async def process_callback_block(callback_query: CallbackQuery):
 
 
 @dispatcher.callback_query_handler(CallbackDataFilter(data='menu'))
-async def process_callback_block(callback_query: CallbackQuery):
+async def process_callback_menu(callback_query: CallbackQuery):
+    """CallbackDataFilter кнопки меню"""
     chat_id = callback_query.message.chat.id
-    user_id = callback_query.message.from_user.id
-    full_name = callback_query.message.from_user.full_name
-    username = callback_query.message.from_user.username
-    loguru.logger.info(f"Вызвано меню пользователем")
+    loguru.logger.info(f"Нажата кнопка меню пользователем")
     await bot.send_sticker(chat_id=chat_id, sticker=menu_sticker)
     await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
-    await bot.send_message(chat_id=chat_id, text=menu, parse_mode='HTML', reply_markup=inline_keyboard)
+    await bot.send_message(chat_id=chat_id, text=menu, parse_mode='HTML', reply_markup=start_menu)
+
+
+@dispatcher.message_handler(commands='subscribe')
+async def subscribe_handler(message: types.Message):
+    """Хэндлер команды /subscribe"""
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    full_name = message.from_user.full_name
+    username = message.from_user.username
+    msg_id = message.message_id
+    await bot.delete_message(chat_id=chat_id, message_id=msg_id)
+    loguru.logger.info(
+        f"Введена команда /subscribe пользователем. Имя: {full_name}, ID: {user_id}, юзернейм: {username}")
+    if is_in_db(chat_id):
+        await bot.send_sticker(chat_id, error_sticker)
+        await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
+        await bot.send_message(chat_id=chat_id, text='❌ Подписка уже оформлена', parse_mode='HTML',
+                               reply_markup=menu_keyboard)
+    else:
+        conn = sqlite3.connect('subscribers.db')  # подключение к базе данных
+        c = conn.cursor()
+
+        c.execute('CREATE TABLE IF NOT EXISTS subscribers (chat_id INTEGER)')  # создание таблицы
+        c.execute('INSERT INTO subscribers (chat_id) VALUES (?)',
+                  (chat_id,))  # добавление в столбец id значения user_id
+
+        conn.commit()
+        conn.close()
+        await bot.send_sticker(chat_id, sub_sticker)
+        await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
+        loguru.logger.info(f"Новая подписка на рассылку: ID{chat_id}")
+        await bot.send_message(chat_id=chat_id, text='✅ Подписка успешно оформлена', parse_mode='HTML',
+                               reply_markup=menu_keyboard)
+
+
+@dispatcher.message_handler(commands='unsubscribe')
+async def unsubscribe_handler(message: types.Message):
+    """Хэндлер команды /unsubscribe"""
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    full_name = message.from_user.full_name
+    username = message.from_user.username
+    msg_id = message.message_id
+    await bot.delete_message(chat_id=chat_id, message_id=msg_id)
+    loguru.logger.info(
+        f"Введена команда /unsubscribe пользователем. Имя: {full_name}, ID: {user_id}, юзернейм: {username}")
+    if is_in_db(chat_id):
+        conn = sqlite3.connect('subscribers.db')
+        c = conn.cursor()
+
+        c.execute("DELETE FROM subscribers WHERE chat_id=?", (chat_id,))
+
+        conn.commit()
+        conn.close()
+        await bot.send_sticker(chat_id, unsub_sticker)
+        await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
+        await bot.send_message(chat_id=chat_id, text='✅ Подписка отменена', parse_mode='HTML',
+                               reply_markup=menu_keyboard)
+    else:
+        await bot.send_sticker(chat_id, error_sticker)
+        await bot.send_chat_action(chat_id, types.ChatActions.TYPING)
+        await bot.send_message(chat_id=chat_id, text='❌ Вы еще не оформляли подписку', parse_mode='HTML',
+                               reply_markup=menu_keyboard)
 
 
 if __name__ == '__main__':
